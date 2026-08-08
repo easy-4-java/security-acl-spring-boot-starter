fbeefc29d5f0be73c05508d09a522de5593de183
wandl-6A72h <hiwepy@gmail.com>
2026-08-08 23:54:28 +0800

    chore: propagate pom standardization, tests, javadoc and @author fix from 4.1.x
    - Add jacoco-maven-plugin 0.8.15 with 90% coverage gate
    - Add central-publishing-maven-plugin 0.11.0
    - Update surefire (enable tests, argLine for JaCoCo, *_Test.java pattern)
    - Fix SCM URLs
    - Copy test files from 4.1.x branch
    - Copy Javadoc improvements from 4.1.x branch
    - Standardize @author to [@Loong Wan](https://github.com/loong10k)
    - Propagated to 3.4.x branch

--- src/test/java/org/springframework/security/boot/acl/BasePermissionExtTest.java
+++ src/test/java/org/springframework/security/boot/acl/BasePermissionExtTest.java
@@ -15,21 +15,24 @@
  */
 package org.springframework.security.boot.acl;
 
-import org.springframework.security.acls.domain.BasePermission;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 /**
- * https://elim.iteye.com/blog/2269021
+ * Unit tests for {{ @link BasePermissionExt }}.
+ *
  * @author [@Loong Wan](https://github.com/loong10k)
+ * @since 1.0.0
  */
+@DisplayName("BasePermissionExt Tests")
+class BasePermissionExtTest {
 
-public class BasePermissionExt extends BasePermission {
-	
-	public BasePermissionExt(int mask) {
-		super(mask);
-	}
-
-	public BasePermissionExt(int mask, char code) {
-		super(mask, code);
-	}
-	
+    @Test
+    @DisplayName("Instance can be created via constructor")
+    void testInstantiation() {
+        BasePermissionExt instance = new BasePermissionExt(null);
+        assertThat(instance).isNotNull();
+    }
 }
